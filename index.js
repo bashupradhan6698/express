78193476506db430e23747245f80a524af156724
Bashu <bashupradhan54@gmail.com>
2023-10-06 23:09:00 +0545

    express

--- index.js
+++ index.js
@@ -3,22 +3,56 @@ const express = require('express')
 const app = express(); //returns object{ }
 
 
-/* 
 
+/* 
+MIDDLEWARE
+-A function which has access to request and response and also next function
+
+next function points out to upcoming valid middleware
+
+-global middleware
+      -route level middleware
+*/
+
+
+function checkAuthentication(req, res, next) {
+  let logged_in = false;
+  if (logged_in) {
+    next();
+  } else {
+    res.status(401).send({ msg: "not logged in" })
+  }
+
+}
+
+// app.use(checkAuthentication)  // global middleware
+
+
+/* Status code
+2
+3
+4
+  400
+  401
+  403
+  404
+5
+
+*/
+/* 
 CRUD : READ CREATE UPDATE DELETE
 request method =>GET POST PUT/PATCH DELETE
 */
 
+app.get("/api/dashboard/", checkAuthentication, (req, res) => {
+  console.log("inside dashboard")
+  res.send({
+    user: 100,
+    sales: 123123,
+  })
+})
 
 
-// const getTodos = (request, response) => {
-//   console.log("return list of todos")
-//   response.send([
-//     { "title": "js", "status": true },
-//     { "title": "css", "status": false },
-
-//   ])
-// }
 
 
 app.get("/", (req, res) => {
@@ -32,7 +66,17 @@ app.get('/api/todos', (req, res) => {
     { "title": "react", "status": false },
 
   ])
-});
+})
+
+
+app.post("/api/todos", (req, res) => {
+  // console.log("save new todos")
+  /* link to database */
+  // console.log(req.body);
+
+  res.send({ "id": "1", "title": "js" });
+
+})
 
 app.listen(8000, () => {        //call back function 
   // (function passed as a variable)
